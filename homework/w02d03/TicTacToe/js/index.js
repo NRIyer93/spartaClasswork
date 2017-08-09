302c95bf24fb4aaa3ba9374f1b6c6964f051b616
Naren Iyer <naren.iyer@hotmail.com>
2017-08-09 21:11:42 +0100

    TicTacToe w/ working squares

--- homework/w02d03/TicTacToe/js/index.js
+++ homework/w02d03/TicTacToe/js/index.js
@@ -1,6 +1,7 @@
 /////////////////////
 //	First Section  //
 ////////////////////
+
 $(function(event){
 // Variables for player1 and player2
 
@@ -11,21 +12,50 @@ var player1choice = [];
 var player2choice = [];
 
 // winning combinations
-var winArray = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]];  
+var winArray = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]]; 
+
+// counter for turns taken
+var turn = 1;
+
+// comparison of player choice with stored winning combinations 
+
+// store player position
+
+
+
 
 // declare all the button-boxes
 var boxes = $("td");
     $(boxes).each(function(index,box){
         onClick(box);
-    
+    })
 
-})
+//////////////////////////
+// 		FUNCTIONS      //
+/////////////////////////
 
-function onClick(box1){
+// function to enable all boxes
+function onClick(box1,){
 	$(box1).on('click', function(){
-		console.log($(box1).attr("data-num"))
-	})
+		console.log($(box1).attr("data-num"));
+		// if statement to determine which player is which
+		if(turn % 2 === 0){
+			$(box1).html('X'); // converts the data attribute to 'X'
+			turn += 1;
+			player1choice.
+		}else {
+			$(box1).html('O'); // converts the data attribute to 'O'
+			turn +=1;
+		}
+	});
 }
+// function which stores number in array
+function onClick(box2,){
+	$(box2).on('click', function(){
+		console.log($(box2).attr("data-num"));
+		}
+}
+
 
 })
 
