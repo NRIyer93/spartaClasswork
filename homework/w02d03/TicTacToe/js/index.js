1129d36b7c22dfb94c5233d18aed2ded7f82136b
Naren Iyer <naren.iyer@hotmail.com>
2017-08-09 21:53:33 +0100

    TicTacToe, iterate array function added

--- homework/w02d03/TicTacToe/js/index.js
+++ homework/w02d03/TicTacToe/js/index.js
@@ -6,20 +6,19 @@ $(function(event){
 // Variables for player1 and player2
 
 // player1 variables
-var player1choice = [];
+var playerXchoice = [];
 
 // player2 variables
-var player2choice = [];
+var playerOchoice = [];
 
 // winning combinations
-var winArray = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]]; 
+var winArrays = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]]; 
 
 // counter for turns taken
 var turn = 1;
 
 // comparison of player choice with stored winning combinations 
 
-// store player position
 
 
 
@@ -42,19 +41,34 @@ function onClick(box1,){
 		if(turn % 2 === 0){
 			$(box1).html('X'); // converts the data attribute to 'X'
 			turn += 1;
-			player1choice.
+			// store move?
+			var position = $(box1).attr("data-num");
+			var indexPosition; 
 		}else {
 			$(box1).html('O'); // converts the data attribute to 'O'
 			turn +=1;
+			// store move?
 		}
 	});
 }
-// function which stores number in array
-function onClick(box2,){
-	$(box2).on('click', function(){
-		console.log($(box2).attr("data-num"));
+
+
+// function to iterate through the winning combinations 
+// and compare with the player's moves
+$(winArrays).each(function(index, array){
+		// if statement to compare choice
+		if(index === playerXchoice){
+			console.log('successful');
 		}
-}
+})
+
+
+// function which stores number in array
+// function onClick(box2,){
+// 	$(box2).on('click', function(){
+// 		console.log($(box2).attr("data-num"));
+// 		})
+// }
 
 
 })
