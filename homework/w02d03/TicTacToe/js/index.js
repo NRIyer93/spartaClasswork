6a4d7c396ad36289831a121b18708e20226e2d09
Naren Iyer <naren.iyer@hotmail.com>
2017-08-10 06:33:32 +0100

    player moves now stored in array

--- homework/w02d03/TicTacToe/js/index.js
+++ homework/w02d03/TicTacToe/js/index.js
@@ -12,7 +12,8 @@ var playerXchoice = [];
 var playerOchoice = [];
 
 // winning combinations
-var winArrays = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]]; 
+var winArrays = [["0","1","2"], ["0","3","6"], ["6","7","8"], 
+				["2","5","8"], ["0","4","8"], ["6","4","2"], ["3","4","5"], ["1","4","7"]]; 
 
 // counter for turns taken
 var turn = 1;
@@ -36,24 +37,31 @@ var boxes = $("td");
 // function to enable all boxes
 function onClick(box1,){
 	$(box1).on('click', function(){
-		console.log($(box1).attr("data-num"));
+		//console.log($(box1).attr("data-num"));
+		
 		// if statement to determine which player is which
 		if(turn % 2 === 0){
 			$(box1).html('X'); // converts the data attribute to 'X'
-			turn += 1;
+			
 			console.log('X turn');
 			// store move?
 			 var position = $(box1).attr("data-num");
+			 console.log("this is X's move: " + position);
 			 var array = playerXchoice.push(position);
-			 console.log(array);
+			 turn += 1;
 		}else {
 			$(box1).html('O'); // converts the data attribute to 'O'
-			turn +=1;
+			
 			console.log('O turn');
 			// store move?
+			var position = $(box1).attr("data-num");
+			console.log("this is O's move: " + position);
 			var array = playerOchoice.push(position);
-			console.log(array);
+			turn +=1;
+			//console.log(array);
 		}
+		console.log(playerXchoice); // shows the array contain
+		console.log(playerXchoice);
 	});
 }
 
