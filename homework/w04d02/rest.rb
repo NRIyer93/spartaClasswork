23439ee63965d1787dfdec12bc394f612119ebcc
Naren Iyer <naren.iyer@hotmail.com>
2017-08-22 19:59:28 +0100

    edit added

--- homework/w04d02/rest.rb
+++ homework/w04d02/rest.rb
@@ -9,24 +9,19 @@ get "/movies/new" do
 	"NEW"
 end
 
+post "/" do
+	"CREATE"
+end
+
 get "/movies/:id" do
 	"SHOW"
 end
 
-put "/movies/:id/edit" do
-	"UPDATE"
+get "/movies/:id/edit" do
+	"EDIT"
 end
 
 
+
+
  
-
-
-
-
-
-
-
-
-# put "/movies/:id/edit" do
-# # 	"UPDATE"
-# # end
