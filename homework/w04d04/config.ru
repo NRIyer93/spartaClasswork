7cca51e65106332614414e7a077bfe64ea4309f9
Naren Iyer <naren.iyer@hotmail.com>
2017-08-24 17:40:17 +0100

    Partials

--- homework/w04d04/config.ru
+++ homework/w04d04/config.ru
@@ -1,2 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require_relative 'controller/recipes_controller.rb'
+
+use Rack::Reloader
+
+use Rack::MethodOverride
+
+run RecipesController
--- homework/w04d04/controller/recipes_controller.rb
+++ homework/w04d04/controller/recipes_controller.rb
@@ -1,3 +1,30 @@
 class RecipesController < Sinatra::Base
+	$recipes = [{
+		id: 0,
+		title: "Devil's Food Cake",
+		content: "jsjjjdj",
+	},
+	{
+		id: 1,
+		title: "Carbonara",
+		content: "jdjdjdjdj",
+	},
+	{
+		id: 2,
+		title: "Lemon Drizzle",
+		content: "jdjdjdjdj"
+	}
+	]
 
+	set :root, File.join(File.dirname(__FILE__), '..')
+
+	set :views, Proc.new { File.join(root, 'views') }
+
+	configure :development do 
+		register Sinatra::Reloader
+	end
+
+	get '/' do
+		"INDEX"
+	end
 end
