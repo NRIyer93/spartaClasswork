86cbd9565ed9316dd26147e48484675286a6edb8
Naren Iyer <naren.iyer@hotmail.com>
2017-08-30 20:54:18 +0100

    Authors and Books show

--- homework/w05d03/bookshelf/db/seeds.rb
+++ homework/w05d03/bookshelf/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Author.create([
+
+# 	{name: "Philip K. Dick"},
+# 	{name: "John le Carre"}
+# ])
+
+# Book.create([
+
+# 	{name: }
+
+
+# ])
