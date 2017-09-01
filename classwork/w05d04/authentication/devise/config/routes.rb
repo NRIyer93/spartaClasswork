c3b6700081b154c8289250a9741b3e69ca4dc0b2
Naren Iyer <naren.iyer@hotmail.com>
2017-09-01 08:47:57 +0100

    new version

--- classwork/w05d04/authentication/devise/config/routes.rb
+++ classwork/w05d04/authentication/devise/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   root 'static#home'
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
